ac54d0c415e28ba385b871195360322524c0571d
Krafi2 <jakub.havlicek5@gmail.com>
2022-01-17 18:17:52 +0100

    Update fish config

--- apps/fish/config.fish
+++ apps/fish/config.fish
@@ -1,11 +1,26 @@
-# Use `please` to repeat the previous command with sudo
+# Fix flatpak environment
+set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
+set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
+
+for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    if test -d $flatpakdir
+        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
+    end
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
 ## Alias section
-alias please='commandline -i "sudo $history[1]";history delete --exact --case-sensitive please' # Sudo last command with `please`
+alias please='eval "sudo $history[1]";history delete --exact --case-sensitive please' # Sudo last command with `please`
 alias cp='cp -i'                                                # Confirm before overwriting something
 alias df='df -h'                                                # Human-readable sizes
 alias free='free -m'                                            # Show sizes in MB
 alias gitu='git add . && git commit && git push'
+alias gits='git status'
+alias gitc='git commit'
+alias gita='git add'
 alias ls='exa'                                                  # Colourful ls
 alias ll='exa -alF'                                             # Colourful ll
 
@@ -38,15 +53,22 @@ set -gx fish_pager_color_prefix        brblue       # Completion common prefix
 set -gx fish_pager_color_progress      brwhite --background=cyan # Completion progress
 
 # Color man pages
-set -x LESS_TERMCAP_md (printf "\e[01;34m")     # Start bold effect
-set -x LESS_TERMCAP_me (printf "\e[0m")         # Stop bold effect
-set -x LESS_TERMCAP_us (printf "\e[01;32m")     # Start underline
-set -x LESS_TERMCAP_ue (printf "\e[0m")         # Stop underline
-set -x LESS_TERMCAP_so (printf "\e[01;40;33m")  # Start reverse video
-set -x LESS_TERMCAP_se (printf "\e[0m")         # Stop  reverse video
-set -x LESS_TERMCAP_mb (printf "\e[01;31m")     # Start blink
+# Force groff to use weird custom escape sequences when run by man. Coloring by less doesn't seem to work otherwise.
+set -gx MANROFFOPT '-c'
+set -gx MANPAGER 'less -R -s -M +Gg'
+set -gx PAGER 'less -R'
 
-# set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"       # Use `bat` as a pager for man
+set -gx LESS_TERMCAP_md (printf "\e[01;34m")     # Start bold effect
+set -gx LESS_TERMCAP_me (printf "\e[0m")         # Stop bold effect
+set -gx LESS_TERMCAP_us (printf "\e[01;32m")     # Start underline
+set -gx LESS_TERMCAP_ue (printf "\e[0m")         # Stop underline
+set -gx LESS_TERMCAP_so (printf "\e[01;40;33m")  # Start reverse video
+set -gx LESS_TERMCAP_se (printf "\e[0m")         # Stop  reverse video
+set -gx LESS_TERMCAP_mb (printf "\e[01;31m")     # Start blink
+
+# Set EDITOR
+set EDITOR nvim
+set VISUAL $EDITOR
 
 set fish_greeting   # Disable welcome message
 
